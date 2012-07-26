905aa927a5d6975301ece2986ccbb4cf6e61457c
zuzu <clown.boy.zuzu@gmail.com>
2012-07-26 15:04:02 +0900

    スライドメニューのマップが正常に表示するように処理

--- kokoTweet/MenuViewController.h
+++ kokoTweet/MenuViewController.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "ECSlidingViewController.h"
 #import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "ECSlidingViewController.h"
 
 @interface MenuViewController : UIViewController <UITableViewDataSource, UITabBarControllerDelegate>
-@property (weak, nonatomic) IBOutlet MKMapView *mapView;
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet UINavigationBar *navBar;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView; //上部に表示するMapKit
+@property (weak, nonatomic) IBOutlet UITableView *tableView;//ページ遷移用のメニューリスト
 @end
--- kokoTweet/MenuViewController.m
+++ kokoTweet/MenuViewController.m
@@ -7,47 +7,88 @@
 //
 
 #import "MenuViewController.h"
-#import <CoreLocation/CoreLocation.h>
 
 @interface MenuViewController()
-@property (nonatomic, strong) NSArray *menuItems;
 @end
 
-@implementation MenuViewController
+// ローカル変数記述領域
+@implementation MenuViewController{
+    __strong  NSArray *menuItems; //メニューリスト用配列。
+                                  //将来的には二次元配列にし遷移先のView名と実際の表示名で分ける。
+}
+
+@synthesize navBar;
 @synthesize mapView;
 @synthesize tableView;
-@synthesize menuItems;
-@synthesize locationManager;
-
-- (void)awakeFromNib
-{
-  self.menuItems = [NSArray arrayWithObjects:@"TimelineViewController", @"SettingDialog", nil];
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
+    // メニューリストを宣言
+    menuItems = [NSArray arrayWithObjects:@"TimelineViewController", @"SettingDialog", nil];
+
+    //スライドメニュー用の設定
     [self.slidingViewController setAnchorRightRevealAmount:280.0f];
     self.slidingViewController.underLeftWidthLayout = ECFullWidth;
     
-    if ([CLLocationManager locationServicesEnabled]) {
-        self.locationManager = [[CLLocationManager alloc] init];
-        self.locationManager.delegate = self;
-        [self.locationManager startUpdatingLocation];
-        
-        [self.mapView setShowsUserLocation:YES];
-
-        NSLog(@"Start updating location.");
-        
-    } else {
-        NSLog(@"The location services is disabled.");
-    }
+    //MapKitのデリゲート及び位置情報取得用処理
+    mapView.delegate = self;
+    [mapView.userLocation addObserver:self forKeyPath:@"location" options:0 context:NULL];
 }
+
+// 通知センターを利用したデータ取得処理
+// 今回は緯度経度を取得している
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    mapView.centerCoordinate = mapView.userLocation.location.coordinate;
+    
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.005, 0.005);
+    MKCoordinateRegion region = MKCoordinateRegionMake(mapView.userLocation.location.coordinate, span);
+    [mapView setRegion:region animated:YES];
+
+    [mapView.userLocation removeObserver:self forKeyPath:@"location"];
+    
+    CLGeocoder *geoCoder = [[CLGeocoder alloc] init];
+    [geoCoder reverseGeocodeLocation:mapView.userLocation.location completionHandler:
+     ^(NSArray* placemarks, NSError* error){
+         NSString *my_str = [[NSString alloc] init];
+         
+         if ([placemarks count] > 0){
+             CLPlacemark *my_placemark = [placemarks objectAtIndex:0];
+             NSArray *address = [my_placemark.addressDictionary valueForKey:@"FormattedAddressLines"];
+             my_str = @"";
+             for(int i=0;i<[address count];i++){
+                 my_str = [my_str stringByAppendingString:[address objectAtIndex:i]];
+             }
+             //頭の郵便番号を除去
+             NSRegularExpression *regexp =
+             [NSRegularExpression regularExpressionWithPattern:@"^〒\\d{3}-\\d{4} (.+)"
+                                                       options:0
+                                                         error:nil];
+             NSTextCheckingResult *match =
+             [regexp firstMatchInString:my_str options:0 range:NSMakeRange(0, my_str.length)];
+             
+             if(match.numberOfRanges > 0) //〒123-4567の場合
+                 my_str = [my_str substringWithRange:[match rangeAtIndex:1]];
+             
+             regexp = [NSRegularExpression regularExpressionWithPattern:@"^\\d{7} (.+)"
+                                                                options:0
+                                                                  error:nil];
+             match =
+             [regexp firstMatchInString:my_str options:0 range:NSMakeRange(0, my_str.length)];
+             
+             if(match.numberOfRanges > 0)
+                 my_str = [my_str substringWithRange:[match rangeAtIndex:1]];
+        }else{
+             my_str = @"(不明)";
+         }
+    }];
+}
+
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)sectionIndex
 {
-  return self.menuItems.count;
+  return menuItems.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -58,14 +99,14 @@
     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
   }
   
-  cell.textLabel.text = [self.menuItems objectAtIndex:indexPath.row];
+  cell.textLabel.text = [menuItems objectAtIndex:indexPath.row];
   
   return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  NSString *identifier = [NSString stringWithFormat:@"%@View", [self.menuItems objectAtIndex:indexPath.row]];
+  NSString *identifier = [NSString stringWithFormat:@"%@View", [menuItems objectAtIndex:indexPath.row]];
 
   UIViewController *newTopViewController = [self.storyboard instantiateViewControllerWithIdentifier:identifier];
   
@@ -80,34 +121,8 @@
 - (void)viewDidUnload {
     [self setMapView:nil];
     [self setTableView:nil];
+    [self setNavBar:nil];
     [super viewDidUnload];
 }
 
-- (void)mapReload:(CLLocation*)location
-{
-    CLLocationCoordinate2D coordinate = location.coordinate;
-    NSLog(@"----------------------------------------------------");
-    NSLog(@"latitude,logitude : %f, %f", coordinate.latitude, coordinate.longitude);
-    NSLog(@"altitude          : %f", location.altitude);
-    NSLog(@"cource            : %f", location.course);
-    NSLog(@"horizontalAccuracy: %f", location.horizontalAccuracy);
-    NSLog(@"verticalAccuracy  : %f", location.verticalAccuracy);
-    NSLog(@"speed             : %f", location.speed);
-    NSLog(@"timestamp         : %@", location.timestamp);
-    MKCoordinateRegion region = MKCoordinateRegionMake([location coordinate], MKCoordinateSpanMake(1.75, 1.75));
-    [self.mapView setCenterCoordinate:[location coordinate]];
-    [self.mapView setRegion:region];
-
-    //[self.locationManager stopUpdatingLocation]; //測位停止
-}
-
-- (void)locationManager:(CLLocationManager *)manager
-    didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-    
-    [self mapReload:newLocation];
-}
-
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
-    NSLog(@"Error: %@", error);
-}
 @end
