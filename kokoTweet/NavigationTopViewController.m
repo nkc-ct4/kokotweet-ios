b18fc2211964e687da940baddd1ec8169419aa0f
hourai <xthstyle@hotmail.co.jp>
2012-07-26 14:20:53 +0900

    サイドメニューに被さる影を設定

--- kokoTweet/NavigationTopViewController.m
+++ kokoTweet/NavigationTopViewController.m
@@ -12,17 +12,17 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-  [super viewWillAppear:animated];
-  
-  if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
-    self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
-  }
-  [self.view addGestureRecognizer:self.slidingViewController.panGesture];
-//    [self.slidingViewController setAnchorRightRevealAmount:280.0f];
+    [super viewWillAppear:animated];
+    
+    if (![self.slidingViewController.underLeftViewController isKindOfClass:[MenuViewController class]]) {
+        self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    }
+    [self.view addGestureRecognizer:self.slidingViewController.panGesture];
+    //    [self.slidingViewController setAnchorRightRevealAmount:280.0f];
     self.view.layer.shadowOpacity = 0.75f;
     self.view.layer.shadowRadius = 10.0f;
     self.view.layer.shadowColor = [UIColor blackColor].CGColor;
-
+    
 }
 
 
